c3e7b340c1d243fa8514618deced14c7256f3e62
suman-coder-123 <sumanpatidar2006@gmail.com>
2025-07-14 14:36:41 +0530

    ju

--- home.js
+++ home.js
@@ -1,1 +1,11 @@
-/
+const togglebtn = document.getElementById("theme-toggle");
+
+togglebtn.addEventListener("click", () => {
+    document.body.classList.toggle("Light-mode");
+
+    if (document.body.classList.contains("Light-mode")) {
+        togglebtn.textContent="Dark Mode";
+    } else {
+        togglebtn.textContent="Light Mode"
+    }
+});
